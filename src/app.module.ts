50e02d1a3f5be3dabf79208f3fef67d1ebe5b54e
Fabian Tenzara <fabiantenzara@gmail.com>
2025-05-14 12:01:04 +0700

    update: api root endpoint and ping endpoint works

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule, UserModule],
-  providers: [PrismaService],
+  providers: [PrismaService, AppService],
   exports: [PrismaService],
+  controllers: [AppController],
 })
 export class AppModule {}
